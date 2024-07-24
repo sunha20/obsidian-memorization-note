19eeb88f23ac4985fe9e61f880aeb095ec7ae802
sunha20 <c.seonha20@gmail.com>
2024-07-25 02:54:14 +0900

    Memorization Note version 1

--- main.ts
+++ main.ts
@@ -1,72 +1,39 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {App, HeadingCache, Notice, Plugin, TFile, TFolder} from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+// interface MyPluginSettings {
+// 	mySetting: string;
+// }
+//
+// const DEFAULT_SETTINGS: MyPluginSettings = {
+// 	mySetting: 'default'
+// }
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	// settings: MyPluginSettings;
 
 	async onload() {
-		await this.loadSettings();
+		// await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+			id: 'create-memorization-note',
+			name: 'Create Memorization Note',
+			callback: async () => {
+				try {
+					const original = new Original(this.app);
+					const memorization = new Memorization(this.app);
+					// @ts-ignore
+					memorization.createNewFile(original.file.basename, original.file.path, original.file.parent, original.getFileHeading())
+				} catch (error) {
+					new Notice(error.message);
 				}
 			}
 		});
 
+
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		// this.addSettingTab(new SampleSettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
@@ -82,53 +49,142 @@ export default class MyPlugin extends Plugin {
 
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+	// async loadSettings() {
+	// 	this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	// }
+	//
+	// async saveSettings() {
+	// 	await this.saveData(this.settings);
+	// }
 }
 
-class SampleModal extends Modal {
+// class ErrorPopup extends Notice {
+// 	error: string;
+// 	constructor(app: App, error: string) {
+// 		super(app);
+// 		this.error = error;
+// 	}
+//
+// 	onOpen() {
+// 		const {contentEl} = this;
+// 		contentEl.setText(this.error);
+// 	}
+//
+// 	onClose() {
+// 		const {contentEl} = this;
+// 		contentEl.empty();
+// 	}
+// }
+
+// class SampleSettingTab extends PluginSettingTab {
+// 	plugin: MyPlugin;
+//
+// 	constructor(app: App, plugin: MyPlugin) {
+// 		super(app, plugin);
+// 		this.plugin = plugin;
+// 	}
+//
+// 	display(): void {
+// 		const {containerEl} = this;
+//
+// 		containerEl.empty();
+//
+// 		new Setting(containerEl)
+// 			.setName('Setting #1')
+// 			.setDesc('It\'s a secret')
+// 			.addText(text => text
+// 				.setPlaceholder('Enter your secret')
+// 				.setValue(this.plugin.settings.mySetting)
+// 				.onChange(async (value) => {
+// 					this.plugin.settings.mySetting = value;
+// 					await this.plugin.saveSettings();
+// 				}));
+// 	}
+// }
+
+class Original {
+	readonly app: App;
+	file: TFile | null = null;
+
 	constructor(app: App) {
-		super(app);
+		this.app = app;
+		const file = this.getActiveFile();
+		if (file == null) {
+			throw new Error("No active file is available");
+		}
+		this.file = file;
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+	// original file
+	getActiveFile(): TFile | null {
+		// workspace의 activeLeaf를 통해 현재 활성화된 leaf를 가져옴
+		const activeFile = this.app.workspace.getActiveFile();
+
+		if (activeFile && activeFile.extension == "md") {
+			return activeFile;
+		}
+		return null;
 	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+	getFileHeading(): HeadingCache[] {
+		if (this.file !== null) {
+			const headings = this.app.metadataCache.getFileCache(this.file)?.headings;
+			return headings ?? [];
+		}
+		return [];
 	}
 }
+
+class Memorization {
+	readonly app: App;
+
+	constructor(app: App) {
+		this.app = app;
+	}
+
+	async createNewFile(originalTitle: string, originalPath: string, folder: TFolder, headings: HeadingCache[]): Promise<TFile> {
+		const pathList: string[] = originalPath.split("/");
+		const titleIdx = pathList.length - 1;
+
+		const newTitle = originalTitle + "_mem ";
+		let num = 1;
+		for (const f of folder.children) {
+			if (f.name.startsWith(newTitle)) {
+				const nameList = f.name.split(" ");
+				const newNum = parseInt(nameList[nameList.length - 1]);
+				if (newNum >= num) {
+					num = newNum + 1;
+				}
+			}
+		}
+
+		pathList[titleIdx] = newTitle + num + ".md";
+		const newPath= pathList.join("/");
+
+		let content = "";
+		let prefix;
+		let head;
+		for (const h of headings) {
+			prefix = "#".repeat(h.level);
+			head = `[[${originalPath}#${h.heading}|${h.heading}]]`;
+			content += `${prefix} ${head}\n\n`;
+		}
+		try {
+			const newFile = await this.app.vault.create(newPath, content);
+
+			this.openFile(newFile);
+
+			return newFile;
+		}
+		catch (error) {
+			new Notice("Failed to create new file:", error)
+		}
+	}
+
+	async openFile(file: TFile) {
+		const leaf = this.app.workspace.getLeaf("tab");
+		await leaf.openFile(file);
+	}
+
+}
+
